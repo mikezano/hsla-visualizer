e43dea14dfde7bdd71b8b5737c56509a335f860d
mikezano <mikezano@gmail.com>
2019-06-13 22:16:33 -0700

    label to grid

--- src/components/ColorGrid.vue
+++ src/components/ColorGrid.vue
@@ -1,19 +1,32 @@
 <template>
-	<div class="grid">
-		<!-- <div class="grid__cell-header"></div>
-		<div class="grid__cell-header">Saturation</div>
-		<div class="grid__cell-header">Light</div>-->
-		<template v-for="i in 11">
-			<template v-for="j in 11">
-				<div class="grid__cell" :style="{backgroundColor: setHslaValues((i-1)*10,(j-1)*10)}"></div>
+	<div class="hsla">
+		<div class="hsla__label--light">
+			<span class="hsla__label--span">0%</span>
+			&larr; Light &rarr;
+			<span class="hsla__label--span">100%</span>
+		</div>
+		<div class="hsla__label--saturation">
+			<span class="hsla__label--span">0%</span>
+			&larr; Saturation &rarr;
+			<span class="hsla__label--span">100%</span>
+		</div>
+		<div class="grid">
+			<template v-for="i in 11">
+				<template v-for="j in 11">
+					<div
+						class="grid__cell"
+						:style="{backgroundColor: setHslaValues((i-1)*10,(j-1)*10)}"
+						:title="setHslaValues((i-1)*10,(j-1)*10)"
+					></div>
+				</template>
 			</template>
-		</template>
+		</div>
 	</div>
 </template>
 
 <script lang="ts">
-import { Component, Vue, Prop } from "vue-property-decorator";
-import { Hsla } from "@/models/Models.ts";
+import { Component, Vue, Prop } from 'vue-property-decorator';
+import { Hsla } from '@/models/Models.ts';
 
 @Component({})
 export default class ColorGrid extends Vue {
@@ -30,7 +43,7 @@ export default class ColorGrid extends Vue {
 	public setHslaValues(saturation: number, light: number): string {
 		const hsla: string = `hsla(${
 			this.color.hue
-		},${saturation}%,${light}%,1)`;
+		}, ${saturation}%, ${light}%,1 )`;
 
 		return hsla;
 	}
@@ -39,17 +52,46 @@ export default class ColorGrid extends Vue {
 
 <style lang="scss" scoped>
 $size: 3rem;
+
+.hsla {
+	position: relative;
+	&__label--light,
+	&__label--saturation {
+		color: black;
+		position: absolute;
+		top: 1rem;
+		font-weight: bold;
+		font-size: 2rem;
+		display: flex;
+		justify-content: center;
+		align-items: center;
+		width: 100%;
+	}
+
+	&__label--span {
+		font-size: 1rem;
+	}
+
+	&__label--saturation {
+		transform: rotateZ(-90deg);
+		left: 1rem;
+		bottom: 0;
+	}
+}
+
 .grid {
 	display: grid;
 	grid-template-columns: repeat(11, calc(50vw / 11));
 	grid-template-rows: repeat(11, calc(50vh / 11));
+	grid-gap: 1rem;
+	padding: 4rem;
 
 	&__cell-header {
 		//border: 1px solid gray;
 	}
 	&__cell {
 		//background-color: gray;
-		//box-shadow: 0.1rem 0.1rem 0.2rem hsla(0, 0, 0, 0.5);
+		box-shadow: 0.1rem 0.1rem 0.2rem hsla(0, 0, 0, 0.5);
 	}
 	&__cell--percent {
 		display: flex;
